1d5677c08706e153ee1fbc6628042f61fb8f24fa
Valery Semenenko <gearmobile@gmail.com>
2025-10-07 23:48:06 +0300

    add lesson 02

--- lesson_02/main.go
+++ lesson_02/main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"errors"
+)
+
 type employee struct {
 	id     int
 	name   string
@@ -17,6 +21,35 @@ type memoryStorage struct {
 	data map[int]employee
 }
 
+func newMemoryStorage() *memoryStorage {
+	return &memoryStorage{
+		data: make(map[int]employee),
+	}
+}
+
+func (m *memoryStorage) insert(e employee) error {
+	m.data[e.id] = e
+
+	return nil
+}
+
+func (m *memoryStorage) get(id int) (employee, error) {
+	e, exists := m.data[id]
+
+	if !exists {
+		return employee{}, errors.New("not_found")
+	}
+
+	return e, nil
+}
+
+func (m *memoryStorage) delete(id int) error {
+	delete(m.data, id)
+
+	return nil
+}
+
 func main() {
-	//
+	var s storage
+	s = newMemoryStorage()
 }
