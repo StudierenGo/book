883e2d43f713af84183816f83924bc3037db7d4d
Valery Semenenko <gearmobile@gmail.com>
2025-10-09 23:27:19 +0300

    add lesson_04

---
+++ lesson_04/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello from Go!")
+	})
+
+	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, r.URL.Query().Get("message"))
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
