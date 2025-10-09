642fc4c5a5358d0a570510a9d0567d5e9c2e2471
Valery Semenenko <gearmobile@gmail.com>
2025-10-10 00:24:29 +0300

    add lesson_05

---
+++ lesson_05/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	router := gin.Default()
+
+	router.POST("/employee")
+	router.GET("/employee/:id")
+	router.PUT("/employee/:id")
+	router.DELETE("employee/:id")
+
+	router.Run(":8080")
+}
